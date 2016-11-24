400ca0821b0f2106c53e1f28777043d507f34067
Dmitri Akatov <akatov@gmail.com>
2016-11-24 14:19:12 +0000

    actually return user data or error for registration#user

---
+++ test/controllers/registration_controller_test.exs
@@ -0,0 +1,30 @@
+defmodule Peepchat.RegistrationControllerTest do
+  use Peepchat.ConnCase
+  alias Peepchat.User
+
+  @valid_attrs %{
+    email: "mike@exaple.com",
+    password: "qwerqwer",
+    password_confirmation: "qwerqwer"
+  }
+
+  @invalid_attrs %{}
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  test "creates and renders resource when data is valid", %{conn: conn} do
+    conn = post conn, registration_path(conn, :create), %{data: %{type: "user",
+                                                                  attributes: @valid_attrs}}
+    assert json_response(conn, 201)["data"]["id"]
+    assert Repo.get_by(User, %{email: @valid_attrs[:email]})
+  end
+
+  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
+    assert_error_sent 400, fn ->
+      conn = post conn, registration_path(conn, :create), %{data: %{type: "user",
+                                                                    attributes: @invalid_attrs}}
+    end
+  end
+end

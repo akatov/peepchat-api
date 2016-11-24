5c1a7db13315cea7d528d0d58ea7d335a7540ba7
Dmitri Akatov <akatov@gmail.com>
2016-11-24 15:21:41 +0000

    adjust tests to JSON API changes

--- test/controllers/registration_controller_test.exs
+++ test/controllers/registration_controller_test.exs
@@ -3,9 +3,9 @@ defmodule Peepchat.RegistrationControllerTest do
   alias Peepchat.User
 
   @valid_attrs %{
-    email: "mike@exaple.com",
-    password: "qwerqwer",
-    password_confirmation: "qwerqwer"
+    "email" => "mike@exaple.com",
+    "password" => "qwerqwer",
+    "password-confirmation" => "qwerqwer"
   }
 
   @invalid_attrs %{}
@@ -15,15 +15,15 @@ defmodule Peepchat.RegistrationControllerTest do
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, registration_path(conn, :create), %{data: %{type: "user",
+    conn = post conn, registration_path(conn, :create), %{data: %{type: "users",
                                                                   attributes: @valid_attrs}}
     assert json_response(conn, 201)["data"]["id"]
-    assert Repo.get_by(User, %{email: @valid_attrs[:email]})
+    assert Repo.get_by(User, %{email: @valid_attrs["email"]})
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
     assert_error_sent 400, fn ->
-      conn = post conn, registration_path(conn, :create), %{data: %{type: "user",
+      conn = post conn, registration_path(conn, :create), %{data: %{type: "users",
                                                                     attributes: @invalid_attrs}}
     end
   end
